f87a08f5b6529b2a89d1d4b10cfc94c68b5f46de
João Pedro <joao.brandao@uniriotec.br>
2020-05-16 22:41:09 -0300

    figma style route

--- api/index.js
+++ api/index.js
@@ -30,5 +30,19 @@ router.get('/image', function(req, res){
     }).catch(err => res.send(err))
 });
 
+router.get('/node', function(req, res){
+  axios.get('https://api.figma.com/v1/files/'+req.query.key+'/nodes?ids='+req.query.ids, {headers: {"X-Figma-Token":req.query.token}})
+    .then(value =>{
+      res.status(200).send(value.data)
+    }).catch(err => res.send(err))
+});
+
+router.get('/style', function(req, res){
+  axios.get('https://api.figma.com/v1/styles/'+req.query.key, {headers: {"X-Figma-Token":req.query.token}})
+    .then(value =>{
+      res.status(200).send(value.data)
+    }).catch(err => res.send(err))
+});
+
 module.exports = router;
 
